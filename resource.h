6b27e78ae61324ff6396c49044ddeac9220ce402
"OscarL" <oscar.lesta@gmail.com>
2015-03-10 04:15:10 -0300

    Initial import of Louai Munajim's Matrix Screen Saver, as available at:
    http://elouai.com/the_matrix_screensaver.php
    
    Link to the source code: http://elouai.com/matrixsrc.zip
    
    From that archive, I've removed only the .psd files.
    
    There is no copyright license on the archive, the source code. But, on the
    mentioned webpage's footer it reads:
    
    "All work by eLouai is licensed under a Creative Commons Attribution 3.0 Unported License"
    
    So I guess that CC BY 3.0 (http://creativecommons.org/licenses/by/3.0/) applies.
    
    When I'm done updating the code, I'll try to contact Louai regarding the license.

---
+++ resource.h
@@ -0,0 +1,33 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by ScriptMatrix.rc
+//
+#define IDI_ICON_MATRIX                 102
+#define IDD_DIALOG_MATRIX               103
+#define IDB_BITMAP_TEST                 104
+#define IDB_BITMAP_TEST_OK              106
+#define IDB_BITMAP_TEST_OK1             108
+#define IDB_BITMAP_TEST_CANCEL          109
+#define IDB_BITMAP_TEST_CANCEL1         110
+#define IDC_EDIT_MAXSTREAMS             1000
+#define IDC_BUTTON_OK                   1001
+#define IDC_BUTTON_CANCEL               1002
+#define IDC_EDIT_BACKTRACE              1003
+#define IDC_EDIT_LEADING                1004
+#define IDC_EDIT_SPACEPAD               1005
+#define IDC_EDIT_SPEEDDELAY             1006
+#define IDC_EDIT_RED                    1007
+#define IDC_EDIT_GREEN                  1008
+#define IDC_EDIT_BLUE                   1009
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        111
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1009
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
